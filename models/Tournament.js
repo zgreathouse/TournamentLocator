a21fba9f61478076be8c193dd1022574bd75be82
zgreathouse <greathouse.zachary@gmail.com>
2018-01-14 15:31:13 -0800

    write tentative tournamentSchema

---
+++ models/Tournament.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const postSchema = require('./Post');
+
+//User Schema
+const tournamentSchema = new Schema({
+  title: String,
+  game: String,
+  tags: [String],
+  tournamentOrganizer: { type: Schema.Types.ObjectId, ref: 'User'},
+  streamLink: String,
+  twitterLink: String,
+  date: Date,
+  startTime: Date,
+  endTime: Date,
+  venueAddress: String,
+  venueFee: Number,
+  entryFee: Number,
+  potBonus: Number,
+  maxEntrants: Number,
+  bannerImage: String,
+  series: Boolean,
+  forum: [postSchema]
+});
+
+mongoose.model('tournaments', tournamentSchema);
