a59fd587a16c50616590604a87b00c63df03e9d9
zgreathouse <greathouse.zachary@gmail.com>
2018-01-14 19:45:13 -0800

    import mlab api key

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 //class Models
 require('./models/User');
 require('./models/Tournament');
 require('./models/Post');
 require('./models/Comment');
+
+mongoose.connect(keys.mongoURI, {
+  useMongoClient: true
+});
 
 //create instance of express
 const app = express();
