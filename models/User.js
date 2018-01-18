8c2a425752c8cfa2f678fa634539644533c3a27a
zgreathouse <greathouse.zachary@gmail.com>
2018-01-17 23:02:08 -0800

    edit database and readme

--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ const tournamentSchema = require('./Tournament');
 //User Schema
 const userSchema = new Schema({
   username: String,                                                     // the user's username
+  title: String,                                                        // the user's title
   city: String,                                                         // city the user is based out of
   travelRange: { type: Number, default: 25 },                           // travel radius specified by the user
   followedGames: [String],                                              // an array of the games the user follows
