4cf900cf98c6445f7c4429742fe86cc2d86660db
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-19 15:13:12 -0800

    removed outdated callbackURL

--- services/googleStrategy.js
+++ services/googleStrategy.js
@@ -18,10 +18,9 @@ passport.deserializeUser((id, done) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-      proxy: true
+      clientID: keys.googleClientID,            //NOT YET IN KEYS
+      clientSecret: keys.googleClientSecret,    //NOT YET IN KEYS
+      callbackURL: '',
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleID: profile.id });
