68da930469197760d465330231b31230dae35962
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-19 15:06:05 -0800

    add google strategy

--- models/User.js
+++ models/User.js
@@ -6,11 +6,15 @@ const tournamentSchema = require('./Tournament');
 //User Schema
 const userSchema = new Schema({
   username: String,                                                     // the user's username
+  googleID: String,                                                     // google ID saved for google OAuth
+  facebookID: String,                                                   // facebook ID saved for google OAuth
+  title: String,                                                        // the user's title
   city: String,                                                         // city the user is based out of
   travelRange: { type: Number, default: 25 },                           // travel radius specified by the user
   followedGames: [String],                                              // an array of the games the user follows
   followedSeries: [String],                                             // an array of different tournament series the user follows
   tournaments: [ tournamentSchema ]                                     // an array of tournaments created/run by the user
 });
+
 
 mongoose.model('users', userSchema);
