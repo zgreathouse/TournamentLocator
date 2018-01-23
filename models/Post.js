466b25fa77be81d86ff8fd0a691ead4c981e3070
zgreathouse <greathouse.zachary@gmail.com>
2018-01-22 18:58:18 -0800

    final clean up and changes before merging to master

--- models/Post.js
+++ models/Post.js
@@ -5,7 +5,7 @@ const commentSchema = require('./Comment');
 
 //Post Schema
 const postSchema = new Schema({
-  _user: { type: Schema.Types.ObjectId, ref: 'User' },                   // id of user who submitted the post
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },                  // id of user who submitted the post
   _tournament: { type: Schema.Types.ObjectId, ref: 'Tournament'},       // id of tournament the post belongs to
   title: String,                                                        // title of the post
   body: String,                                                         // body of the post
