e30432053e3580124fadc57fdbe8cfcd0ad717b1
zgreathouse <greathouse.zachary@gmail.com>
2018-01-23 22:29:44 -0800

    build redux boilerplate and redux cycle for the auth slice of state with an authReducer, and fetchUser action

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,22 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//components
+import App from './components/App';
+import reducers from './reducers';
+
+//for testing backend api:
+import axios from 'axios';
+window.axios = axios;
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root')
+);
