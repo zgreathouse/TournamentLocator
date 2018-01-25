df038fd4d16e17a47f21ecff52ec4206a961f135
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-24 22:27:04 -0800

    started POST /api/tournaments route

--- routes/tournamentRoutes.js
+++ routes/tournamentRoutes.js
@@ -18,11 +18,34 @@ module.exports = app => {
     res.send(tournaments);
   });
 
-/*
+
   // Create a new tournament in the database
-  app.post('/api/tournaments', );
+  app.post('/api/tournaments', requireLogin, async (req, res) => {
+    const tournament = new Tournament({
+      _user: req.user.id,
+      title: req.body.title,
+      game: req.body.game,
+      tags: req.body.tags,
+      description: req.body.description,
+      streamLink: req.body.streamLink,
+      twitterLink: req.body.twitterLink,
+      startTime: req.body.startTime,
+      endTime: req.body.endTime,
+      venueAddress: req.body.venueAddress,
+      venueFee: req.body.venueFee,
+      entryFee: req.body.entryFee,
+      sponsors: req.body.sponsors,
+      potBonus: req.body.potBonus,
+      maxEntrants: req.body.maxEntrants,
+      bannerImage: req.body.bannerImage,
+      series: req.body.series,
+      forum: []
+    })
 
+    
+  });
 
+/*
   // Update (edit) an existing tournament
   app.patch('/api/tournaments', );
 
@@ -32,3 +55,22 @@ module.exports = app => {
 
 */
 }
+
+/*
+const { title,
+        game,
+        tags,
+        description,
+        streamLink,
+        twitterLink,
+        startTime,
+        endTime,
+        venueAddress,
+        venueFee,
+        entryFee,
+        sponsors,
+        potBonus,
+        maxEntrants,
+        bannerImage,
+        series } = req.body;
+*/
