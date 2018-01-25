302aa3c3d185f37c8c0c5e72c5d82f742a52c62d
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-24 21:45:35 -0800

    add first drafts of /api/tournaments and /api/:userId/tournaments

--- routes/tournamentRoutes.js
+++ routes/tournamentRoutes.js
@@ -5,17 +5,20 @@ const requireLogin = ('../middlewares/requireLogin');
 const Tournament = mongoose.model('tournaments');
 
 module.exports = app => {
-/*
-  Scafolding for Routes
-
   // Get an index of all the up and coming tournaments (and current)
-  app.get('/api/tournaments', );
+  app.get('/api/tournaments', requireLogin, (req, res) => {
+    res.send(req.user.tournaments);
+  });
 
 
   // Get an index of all the tournaments created by a user
-  app.get('/api/:userId/tournaments/', );
+  app.get('/api/:userId/tournaments/', requireLogin, async (req, res) => {
+    const tournaments = await Tournament.find({ _user: req.user.id })
 
+    res.send(tournaments);
+  });
 
+/*
   // Create a new tournament in the database
   app.post('/api/tournaments', );
 
