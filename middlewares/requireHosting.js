ebd790249f275e227c9ae937b2cf0c55aa7a070d
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-25 20:25:25 -0800

    added first draft of requireHosting middleware

---
+++ middlewares/requireHosting.js
@@ -0,0 +1,8 @@
+//custom middleware for determining if the user is the host of a tournament
+module.exports = (req, res, next) => {
+  if (req.user.tournaments.includes(req.user.id) {
+    return res.status(401).send({ error: "You can't delete this tournament."});
+  }
+
+  next();
+};
