ba1f6f6b081f50d1b57570ec267ffccd79e4b47a
zgreathouse <greathouse.zachary@gmail.com>
2018-01-27 22:33:41 -0800

    minor styling to logout <a> tag

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -19,7 +19,7 @@ class Header extends Component {
         ];
       default:
         return (
-          <li key={3}><a href="/api/logout">Logout</a></li>
+          <li key={3}><a href="/api/logout" style={{ marginRight: "10px"}}>Logout</a></li>
         );
     }
   }
