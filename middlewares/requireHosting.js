3610373e620f3baeb3a94f93a4e4c1be0a30782e
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-29 21:38:34 -0800

    debugging route error

--- middlewares/requireHosting.js
+++ middlewares/requireHosting.js
@@ -1,6 +1,6 @@
 //custom middleware for determining if the user is the host of a tournament
 module.exports = (req, res, next) => {
-  if (!req.user.tournaments.includes(req.tournament) {
+  if(!req.user.tournaments.includes(req.tournament)) {
     return res.status(401).send({ error: "You can't delete this tournament."});
   }
 
--- routes/tournamentRoutes.js
+++ routes/tournamentRoutes.js
@@ -1,60 +1,58 @@
 const mongoose = require('mongoose');
 
-const requireLogin = ('../middlewares/requireLogin');
-const requireHosting = ('../middlewares/requireHosting');
+const requireLogin = require('../middlewares/requireLogin');
+const requireHosting = require('../middlewares/requireHosting');
 
 const Tournament = mongoose.model('tournaments');
 
 module.exports = app => {
   // Get an index of all the up and coming tournaments (and current)
-  app.get('/api/tournaments', async (req, res) => {
+  app.get('/api/tournaments', requireLogin, async (req, res) => {
     try {
       const results = await Tournament.find({});
       res.send(results);
     } catch (err) {
-      throw err;
-    }
-  });
-
-
-  // Get an index of all the tournaments created by a user
-  app.get('/api/:userId/tournaments/', requireLogin, async (req, res) => {
-    res.send(req.user.tournaments);
-  });
-
-
-  // Create a new tournament in the database
-  app.post('/api/tournaments', requireLogin, async (req, res) => {
-    const tournament = new Tournament({
-      _user: req.user.id,
-      title: req.body.title,
-      game: req.body.game,
-      tags: req.body.tags,
-      description: req.body.description,
-      streamLink: req.body.streamLink,
-      twitterLink: req.body.twitterLink,
-      startTime: req.body.startTime,
-      endTime: req.body.endTime,
-      venueAddress: req.body.venueAddress,
-      venueFee: req.body.venueFee,
-      entryFee: req.body.entryFee,
-      sponsors: req.body.sponsors,
-      potBonus: req.body.potBonus,
-      maxEntrants: req.body.maxEntrants,
-      bannerImage: req.body.bannerImage,
-      series: req.body.series,
-      forum: []
-    })
-
-    try {
-      req.user.tournaments.push(tournament);
-      await tournament.save();
-      const user = await req.user.save();
-      res.send(user);
-    } catch (err) {
       res.status(422).send(err)
     }
+  });
 
+  //Get an index of all the tournaments created by a user
+  app.get('/api/:userId/tournaments', requireLogin, (req, res) => {
+     res.send(req.user.tournaments);
+   });
+
+
+   // Create a new tournament in the database
+  app.post('/api/tournaments', requireLogin, async (req, res) => {
+     const tournament = new Tournament({
+       _user: req.user.id,
+       title: req.body.title,
+       game: req.body.game,
+       tags: req.body.tags,
+       description: req.body.description,
+       streamLink: req.body.streamLink,
+       twitterLink: req.body.twitterLink,
+       startTime: req.body.startTime,
+       endTime: req.body.endTime,
+       venueAddress: req.body.venueAddress,
+       venueFee: req.body.venueFee,
+       entryFee: req.body.entryFee,
+       sponsors: req.body.sponsors,
+       potBonus: req.body.potBonus,
+       maxEntrants: req.body.maxEntrants,
+       bannerImage: req.body.bannerImage,
+       series: req.body.series,
+       forum: []
+     })
+
+     try {
+       req.user.tournaments.push(tournament);
+       await tournament.save();
+       const user = await req.user.save();
+       res.send(user);
+     } catch (err) {
+       res.status(422).send(err)
+     }
   });
 
 /*
@@ -68,9 +66,7 @@ module.exports = app => {
     Tournament.findByIdAndRemove(req.tournament.id, (err) => {});
     res.redirect('/');
   });
-
-}
-
+};
 /*
 const { title,
         game,
