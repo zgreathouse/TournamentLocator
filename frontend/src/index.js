1d0d6f6d5c798dee71bbc3b1bc14d995297ed9c7
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-29 20:19:39 -0800

    unistalled axios from ./ and installed in ./frontend. wired to widow to test routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//testing
+import axios from 'axios';
+window.axios = axios;
 
 ReactDOM.render(<App />, document.getElementById('root'));
