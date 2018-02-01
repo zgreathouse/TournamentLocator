06adf28c380b027200624270f980a7d8adfffede
zgreathouse <greathouse.zachary@gmail.com>
2018-01-31 22:07:44 -0800

    build skeltons for TournamentIndex and TournamentIndexItem components

---
+++ frontend/src/components/tournamentComponents/TournamentIndex.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TournamentIndex extends Component {
+  render() {
+    return (
+      <div>Tournament Index</div>
+    )
+  }
+}
+
+export default TournamentIndex;
---
+++ frontend/src/components/tournamentComponents/TournamentIndexItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TournamentIndexItem extends Component {
+  render() {
+    return (
+      <div>Tournament Index item</div>
+    )
+  }
+}
+
+export default TournamentIndexItem;
