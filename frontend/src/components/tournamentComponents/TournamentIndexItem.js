b6fb3d02abc3d7e1f55d9b275e6babce9bb69860
zgreathouse <greathouse.zachary@gmail.com>
2018-02-02 13:39:04 -0800

    fix syntax errors in TournamentIndexItem component

--- frontend/src/components/tournamentComponents/TournamentIndexItem.js
+++ frontend/src/components/tournamentComponents/TournamentIndexItem.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const TournamentIndexItem = ({ tournament }) => {
-  return (
-    <button style={{marginTop: "30px"}}>
-      <div style={{color: "#403f42", width: "37.49em", height: "6em"}}>
-        <h5 style={{textAlign: "left"}}>{tournament.title}</h5>
-        <p className="left">Game: {tournament.game}</p>
-        <p className="right">{tournament.date}</p>
-      </div>
-    </button>
-  )
+class TournamentIndexItem extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { tournament } = this.props;
+
+    return (
+      <button style={{marginTop: "30px"}}>
+        <div style={{color: "#403f42", width: "37.49em", height: "5em"}}>
+          <h5 style={{textAlign: "left"}}>{tournament.title}</h5>
+          <p className="left">Game: {tournament.game}</p>
+          <p className="right">{tournament.date}</p>
+        </div>
+      </button>
+    )
+  }
 }
 
 export default TournamentIndexItem;
