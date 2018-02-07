ce21e2ed55408ec8c82287e73e8f9236da9b3d16
SDRyan9 <stonedouglasryan@gmail.com>
2018-02-06 17:00:15 -0800

    first draft of full CRUD for Posts added and tested

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,15 +1,108 @@
+const mongoose = require('mongoose');
+const Path = require('path-parser');
+const { URL } = require('url');
+
+const requireLogin = require('../middlewares/requireLogin');
+const requireUsername = require('../middlewares/requireUsername');
+
+const Tournament = mongoose.model('tournaments');
+const Post = mongoose.model('posts');
+
 module.exports = app => {
   // Get an index of all the the posts for a given tournament
-  app.get('/api/posts', );
+  app.get('/api/posts/:tournamentId', async (req, res) => {
+    const p = new Path('/api/posts/:tournamentId');
+    const match = p.test(req.url);
+
+    try {
+      const allPosts = await Post.find({ _tournament: match.tournamentId })
+
+      if(allPosts.length === 0){
+        res.send({emptyMessage: "No Post Yet"})
+      }
+      res.send(allPosts.reverse());
+    } catch(err){
+      res.status(422).send(err);
+    }
+  });
 
   // Create a new post to be added to a Tournament’s collection of posts
-  app.post('/api/posts', );
+  app.post('/api/posts/:tournamentId', requireLogin, requireUsername, async (req, res) => {
+    const p = new Path('/api/posts/:tournamentId');
+    const match = p.test(req.url);
 
+    const tournament = await Tournament.findOne({ _id: match.tournamentId });
+
+    const post = new Post({
+      _user: req.user.id,
+      _tournament: tournament.id,
+      title: req.body.title,
+      body: req.body.body,
+      datePosted: new Date(),
+      comments: []
+    });
+    try {
+      tournament.forum.unshift(post.id);
+      await post.save();
+      const newTournament = await tournament.save();
+      res.send(newTournament)
+    } catch(err){
+      res.status(422).send(err);
+    }
+  });
 
   // Update (edit) an existing post
-  app.patch('/api/posts', );
+  app.patch('/api/posts/:postId', requireLogin, async (req, res) => {
+    const p = new Path('/api/posts/:postId')
+    const match = p.test(req.url);
+
+    const post = await Post.findOne({ _id: match.postId })
+    try {
+      let userId = post._user.toString();
+      if(req.user.id !== userId) {
+        return res.status(401).send({ error: "You can't edit this post."});
+      }
+
+      Post.update({
+        _id: post.id },
+        { $set: {
+          title: req.body.title,
+          body: req.body.body,
+          }
+        }
+      ).exec();
+
+      res.send(req.user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
+  });
 
 
   // Delete an existing post
-  app.delete('/api/posts', );
+  app.delete('/api/posts/:postId', requireLogin, async (req, res) => {
+    const p = new Path('/api/posts/:postId')
+    const match = p.test(req.url);
+
+    const post = await Post.findOne({ _id: match.postId })
+    const tournament = await Tournament.findOne({ _id: post._tournament })
+
+    let userId = post._user.toString();
+    if(req.user.id !== userId) {
+      return res.status(401).send({ error: "You can't edit this post."});
+    }
+
+    try {
+      let updatedPostList = tournament.forum.filter(ele => {
+        return ele !== post.id.toString();
+      });
+
+      Post.findByIdAndRemove(post.id).exec();
+      tournament.forum = updatedPostList;
+      const newTournament = await tournament.save();
+      res.send(newTournament)
+    } catch (err) {
+      res.status(422).send(err);
+    }
+  });
 }
