9f47b078f5dc21709f692bd54f58b6d033885356
zgreathouse <greathouse.zachary@gmail.com>
2018-02-12 20:51:32 -0800

    clean up redux action files

--- frontend/src/actions/tournamentActions.js
+++ frontend/src/actions/tournamentActions.js
@@ -3,9 +3,6 @@ import axios from 'axios';
 //constants
 export const FETCH_TOURNAMENTS  = 'FETCH_TOURNAMENTS';
 export const FETCH_TOURNAMENT   = 'FETCH_TOURNAMENT';
-// export const CREATE_TOURNAMENT  = 'CREATE_TOURNAMENT';
-// export const EDIT_TOURNAMENT    = 'EDIT_TOURNAMENT';
-// export const DELETE_TOURNAMENT  = 'DELETE_TOURNAMENT';
 
 //action which fetches all of the tournaments from the database
 export const fetchTournaments = () => async dispatch => {
@@ -26,24 +23,3 @@ export const fetchTournament = id => async dispatch => {
     payload: res.data
   });
 };
-
-// export const createTournament = tournament => dispatch => {
-//
-//   dispatch({
-//     type: CREATE_TOURNAMENT
-//   })
-// }
-//
-// export const editTournament = tournament => dispatch => {
-//
-//   dispatch({
-//     type: EDIT_TOURNAMENT
-//   })
-// }
-//
-// export const deleteTournament = tournament => dispatch => {
-//
-//   dispatch({
-//     type: DELETE_TOURNAMENT
-//   })
-// }
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //constants
 export const FETCH_USER = 'FETCH_USER';
 
-//action fetches the current user
+//action which fetches the current user
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
 
