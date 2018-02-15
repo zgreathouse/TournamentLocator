f2af833af7235334741fa1fab0af0825dd547434
zgreathouse <greathouse.zachary@gmail.com>
2018-02-15 15:34:22 -0800

    refactor TournamentForm component, creak out the Field to its own component, as well as the submit and cancel buttons

---
+++ frontend/src/components/tournamentComponents/tournamentForm/formField.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const FormField = (field) => {
+  const { touched, error } = field.meta;
+  const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
+  return (
+    <div
+      className={className}
+      style={{margin: "0 50px 10px 50px"}}
+    >
+      <label>{field.label}</label>
+      <input
+        className="form-control"
+        type="text"
+        {...field.input}
+      />
+      <div
+        className="text-help"
+        style={{fontStyle: "italic", color: "#2bbbad"}}
+      >
+        {touched && error}
+      </div>
+    </div>
+  )
+}
+
+export default FormField;
--- frontend/src/components/tournamentComponents/tournamentForm/index.js
+++ frontend/src/components/tournamentComponents/tournamentForm/index.js
@@ -1,47 +1,20 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { createTournament } from '../../../actions/tournamentActions';
 import { FIELDS, unrequiredFields } from '../../../util/formFields';
 
+//components
+import FormField from './formField';
+import SubmitButton from './submitButton';
+import CancelButton from './cancelButton';
+
 class TournamentForm extends Component {
-  renderField(field) {
-    const { touched, error } = field.meta;
-    const className = `form-group ${touched && error ? 'has-danger' : ''}`
-
-    return (
-      <div
-        className={className}
-        style={{margin: "0 50px 10px 50px"}}
-      >
-        <label>{field.label}</label>
-        <input
-          className="form-control"
-          type="text"
-          {...field.input}
-        />
-        <div
-          className="text-help"
-          style={{fontStyle: "italic", color: "#2bbbad"}}
-        >
-          {touched && error}
-        </div>
-      </div>
-    )
-  }
-
   renderFields() {
     return _.map(FIELDS, ({ label, name, type }) => {
       return (
-        <Field
-          key={name}
-          component={this.renderField}
-          type={type}
-          label={label}
-          name={name}
-        />
+        <Field key={name} component={FormField} type={type} label={label} name={name} />
       );
     })
   }
@@ -61,19 +34,8 @@ class TournamentForm extends Component {
         style={{margin: "50px 0 80px 0"}}
       >
         {this.renderFields()}
-        <div>
-          <button
-            type="submit"
-            className="btn left"
-            style={{marginLeft: "50px"}}
-            text="submit"
-          >Submit</button>
-          <Link
-            to="/"
-            className="btn right"
-            style={{marginRight: "50px"}}
-          >Cancel</Link>
-        </div>
+        <SubmitButton />
+        <CancelButton />
       </form>
     )
   }
