a437b014a70370ca00e890f09b4f06d9bc9ca7ff
zgreathouse <greathouse.zachary@gmail.com>
2018-02-15 16:05:13 -0800

    clean up TournamentForm component

--- frontend/src/components/tournamentComponents/tournamentForm/formField.js
+++ frontend/src/components/tournamentComponents/tournamentForm/formField.js
@@ -5,20 +5,10 @@ const FormField = (field) => {
   const className = `form-group ${touched && error ? 'has-danger' : ''}`
 
   return (
-    <div
-      className={className}
-      style={{margin: "0 50px 10px 50px"}}
-    >
+    <div className={className} style={{margin: "0 50px 10px 50px"}}>
       <label>{field.label}</label>
-      <input
-        className="form-control"
-        type="text"
-        {...field.input}
-      />
-      <div
-        className="text-help"
-        style={{fontStyle: "italic", color: "#2bbbad"}}
-      >
+      <input className="form-control" type="text" {...field.input}/>
+      <div className="text-help" style={{fontStyle: "italic", color: "#2bbbad"}}>
         {touched && error}
       </div>
     </div>
