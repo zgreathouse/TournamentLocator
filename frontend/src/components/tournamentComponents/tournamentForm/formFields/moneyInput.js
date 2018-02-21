8228e635d4825b7fcd5037eba4f404969ec2124a
zgreathouse <greathouse.zachary@gmail.com>
2018-02-20 17:03:36 -0800

    clean up moneyInput component file

--- frontend/src/components/tournamentComponents/tournamentForm/formFields/moneyInput.js
+++ frontend/src/components/tournamentComponents/tournamentForm/formFields/moneyInput.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
 const MoneyInput = (field) => {
-  const { touched, error } = field.meta;
-  //change styling based on whether the input has been selected
-  const className = `form-group ${touched && error ? 'has-danger' : ''}`;
-
   return (
-    <div className={className} style={{margin: "0 250px 10px 130px"}}>
+    <div style={{margin: "0 250px 10px 130px"}}>
       <label>{field.label}</label>
       <input type="number" color="#403f42" {...field.input}/>
     </div>
