2ee1325263723a4bf21a3634097d922a7480dff3
zgreathouse <greathouse.zachary@gmail.com>
2018-02-27 23:57:09 -0800

    change references to field into this.props instead inside of the SeriesInput and FileInput components, because of class change we are no longer passing in the props as 'field'

--- frontend/src/components/tournamentComponents/tournamentForm/formFields/fileInput.js
+++ frontend/src/components/tournamentComponents/tournamentForm/formFields/fileInput.js
@@ -9,12 +9,14 @@ import React, { Component } from 'react';
 
 class FileInput extends Component {
   render() {
+    const { label, input } = this.props;
+
     return (
       <div style={{margin: "0 250px 15px 130px"}}>
         <label htmlFor="banner" style={{marginRight: "10px"}}>
-          {field.label}
+          {label}
         </label>
-        <input type="file" color="#403f42" {...field.input} value=""/>
+        <input type="file" color="#403f42" {...input} value=""/>
       </div>
     )
   }
--- frontend/src/components/tournamentComponents/tournamentForm/formFields/seriesInput.js
+++ frontend/src/components/tournamentComponents/tournamentForm/formFields/seriesInput.js
@@ -5,16 +5,17 @@ import React, { Component } from 'react';
 // figure out how value is served up to form
 
 class SeriesInput extends Component {
-
   render() {
+    const { label, input } = this.props;
+
     return (
       <div style={{margin: "0 250px 10px 130px"}}>
-        <p style={{marginRight: "10px", color: "#9e9e9e"}}>Is your tournament held weekly?</p>
+        <p style={{marginRight: "10px", color: "#9e9e9e"}}>{label}</p>
         {/*'yes' radio button*/}
-        <input type="radio" id="seriesYes" value="true" color="#403f42" {...field.input} />
+        <input type="radio" id="seriesYes" value="true" color="#403f42" {...input} />
         <label htmlFor="seriesYes" style={{marginRight: "10px"}}>Yes</label>
         {/*'no' radio button*/}
-        <input type="radio" id="seriesNo" value="false" color="#403f42" checked {...field.input}/>
+        <input type="radio" id="seriesNo" value="false" color="#403f42" checked {...input}/>
         <label htmlFor="seriesNo" style={{marginRight: "10px"}}>No</label>
       </div>
     )
