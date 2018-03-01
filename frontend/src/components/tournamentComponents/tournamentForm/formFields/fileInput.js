e25edffb6bd3f5a0e45994fc51badcd0fba3dcd6
zgreathouse <greathouse.zachary@gmail.com>
2018-02-28 23:08:13 -0800

    add setImage helper method to FileInput component, value of input is currently not being updated to have a file.

--- frontend/src/components/tournamentComponents/tournamentForm/formFields/fileInput.js
+++ frontend/src/components/tournamentComponents/tournamentForm/formFields/fileInput.js
@@ -8,6 +8,16 @@ add a helper method to handle updating the value of the field on file select
 import React, { Component } from 'react';
 
 class FileInput extends Component {
+
+  setImage(e) {
+    const file = e.currentTarget.files[0];
+    let fileReader = new FileReader();
+
+    if (file) {
+      fileReader.readAsDataURL(file);
+    }
+  }
+
   render() {
     const { label, input } = this.props;
 
@@ -16,7 +26,7 @@ class FileInput extends Component {
         <label htmlFor="banner" style={{marginRight: "10px"}}>
           {label}
         </label>
-        <input type="file" color="#403f42" {...input} value=""/>
+        <input type="file" color="#403f42" {...input} value="" onChange={this.setImage}/>
       </div>
     )
   }
