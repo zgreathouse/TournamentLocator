01e5b3d7a64000a788fa81259718e60111650002
zgreathouse <greathouse.zachary@gmail.com>
2018-03-08 12:56:22 -0800

    add devNotes.js file to util folder, add notes to file about what is left to be completed on this branch and what is being pushed out as its own feature

---
+++ frontend/src/util/devNotes.js
@@ -0,0 +1,15 @@
+/*
+
+TODO
+
+- figure out how to save and present the dates as entered by the user
+(currently the date isn't exact to the date being inputted on the form)
+
+- pushing the fileInput and seriesInput (on the TournamentForm) to be done
+on different branches when implementing those specific features
+
+- figure out how to edit the description so that when the description is
+presented on the tournament show page it implements the newlines
+
+
+*/
