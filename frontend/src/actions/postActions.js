296ae8eceafa06ff71d369f9b5ddf33eae32401e
zgreathouse <greathouse.zachary@gmail.com>
2018-03-26 20:53:01 -0700

    create fetchPosts action

--- frontend/src/actions/postActions.js
+++ frontend/src/actions/postActions.js
@@ -4,3 +4,13 @@ export const FETCH_POSTS  = 'FETCH_POSTS';
 export const CREATE_POST  = 'CREATE_POST';
 export const UPDATE_POST  = 'UPDATE_POST';
 export const DELETE_POST  = 'DELETE_POST';
+
+//action which fetches all of the posts for a specific tournament
+export const fetchPosts = (tournamentID) => async dispatch => {
+  const res = await axios.get(`/api/posts/${tournamentID}`);
+
+  dispatch({
+    type: FETCH_POSTS,
+    payload: res.data
+  });
+};
