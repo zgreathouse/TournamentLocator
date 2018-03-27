a37019b8eb7d0b6ea5b58c52de6faee7377eb85d
zgreathouse <greathouse.zachary@gmail.com>
2018-03-26 20:46:31 -0700

    initial commit- create commentActions and postActions files and create constants for each action type, import axios into both files

---
+++ frontend/src/actions/commentActions.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export const FETCH_COMMENTS  = 'FETCH_COMMENTS';
+export const CREATE_COMMENT  = 'CREATE_COMMENT';
+export const DELETE_COMMENT  = 'DELETE_COMMENT';
---
+++ frontend/src/actions/postActions.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const FETCH_POSTS  = 'FETCH_POSTS';
+export const CREATE_POST  = 'CREATE_POST';
+export const UPDATE_POST  = 'UPDATE_POST';
+export const DELETE_POST  = 'DELETE_POST';
