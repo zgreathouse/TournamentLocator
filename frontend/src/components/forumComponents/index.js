b690ea9b64e241e736a726d0a156e4c15f8d6030
zgreathouse <greathouse.zachary@gmail.com>
2018-03-30 11:29:40 -0700

    write comment in PostIndex.js file explaining issue with data not persisting when refreshing the forum page

--- frontend/src/components/forumComponents/index.js
+++ frontend/src/components/forumComponents/index.js
@@ -1,3 +1,11 @@
+/*
+TODO
+  we access the Tournament ID by accessing the ID of the selecte
+  tournament. selectedTournament data is not persisting on refresh.
+  So the selected tournament and posts are reset to nothing when
+  refreshing the forum.
+*/
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../../actions/postActions';
