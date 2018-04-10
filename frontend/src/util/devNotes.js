b4f52dcbbec1c092a549616b221bf5b441224803
zgreathouse <greathouse.zachary@gmail.com>
2018-04-09 22:59:51 -0700

    add todos to devNotes

--- frontend/src/util/devNotes.js
+++ frontend/src/util/devNotes.js
@@ -1,5 +1,10 @@
 /*
 TODO
 
+  * Set up logic so that the tournament delete button is only made
+  visible to the author
+
+  * Create Post edit form
+    * set up edit form to be prepopulated with existing values
 
 */
