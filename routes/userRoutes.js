9e2b23185d5daf67ae6ef85307616f0cb15589fa
SDRyan9 <stonedouglasryan@gmail.com>
2018-04-21 15:03:15 -0700

    finsihed scaffolding

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,11 @@ const requireLogin = require('../middlewares/requireLogin');
 const User = mongoose.model('users');
 
 module.exports = app => {
-  app.patch('/api/users/:userId', async (req, res) =>{
-    
+  app.patch('/api/users/:userId', requireLogin, async (req, res) =>{
+
+  })
+
+  app.delete('/api/users/:userId', requireLogin, async (req, res) =>{
+
   })
 };
