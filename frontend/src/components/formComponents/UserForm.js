98121c2c662c05253391353d8e4b4b6f70ae8c25
zgreathouse <greathouse.zachary@gmail.com>
2018-04-21 19:34:20 -0700

    write userFormFields helper file, build out UserForm, write validations on UserForm

--- frontend/src/components/formComponents/UserForm.js
+++ frontend/src/components/formComponents/UserForm.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+// import { editUser } from '../../actions/userActions';
+import { requireCommas } from '../../util/helperFunctions';
+import { FIELDS, unrequiredFields } from '../../util/userFormFields';
+
+//components
+import TextInput from './formFields/textInput';
+import SubmitButton from './formButtons/submitButton';
+import CancelButton from './formButtons/cancelButton';
 
 class UserForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name, type }) => {
+      <Field key={name} component={TextInput} type={type} label={label} name={name} />
+    });
+  }
+
+  onSubmit(values) {
+    // this.props.editUser(values, () => {
+    //   this.props.history.push('/tournaments');
+    // });
+    console.log(values);
+    console.log("submitted");
+  }
+
   render() {
     return (
-      <div>User Form</div>
+      <div className="tournament-form-container">
+        <h2>User Form</h2>
+        <form
+          className="tournament-form"
+          onSubmit={handleSubmit(this.onSubmit.bind(this))}
+        >
+          {this.renderFields()}
+          <div className="form-buttons">
+            <SubmitButton />
+            <CancelButton route="/tournaments"/>
+          </div>
+        </form>
+      </div>
     )
   }
 }
 
-export default UserForm;
+//form validations
+const validate = values => {
+  const errors = {};
+  const requiredFields = _.omit(FIELDS, unrequiredFields);
+
+  //require how list input values are formatted to be converted to arrays later
+  errors.followedGames = requireCommas(values.followedGames || '');
+  errors.followedSeries = requireCommas(values.followedSeries || '');
+
+  //validate inputs
+  _.each(requiredFields, ({name, errorMessage}) => {
+    if (!values[name]) {
+      errors[name] = `Please enter ${errorMessage}.`;
+    }
+  });
+
+  return errors;
+}
+
+export default reduxForm({
+  fields: _.keys(FIELDS),
+  form: 'UserForm',
+  validate
+})(connect(null)(UserForm));
