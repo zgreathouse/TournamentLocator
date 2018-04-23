3dc97016e50bf9117827d4b0e3b4f1b246270dfd
zgreathouse <greathouse.zachary@gmail.com>
2018-04-22 19:21:28 -0700

    update dev notes for steps on what to complete on this branch

--- frontend/src/util/devNotes.js
+++ frontend/src/util/devNotes.js
@@ -1,6 +1,17 @@
 /*
 TODO
 
+  * create user profile page
+    * create link on page to edit profile
+  * create user edit form
+    * create multi input (require commas is not cutting it)
+  * create link to user profile in header
+  * create redux cycle for editing and deleting users
+  * create delete account button option
+
+
+
+
   * Create Post edit form
     * set up edit form to be prepopulated with existing values
 
