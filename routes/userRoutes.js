24405422a0e4621bab18c0426c8ad52d8efd6480
SDRyan9 <stonedouglasryan@gmail.com>
2018-04-23 20:16:10 -0700

    first draft of PATCH route complete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,36 @@
 const mongoose = require('mongoose');
-const Path = require('path-parser');
-const { URL } = require('url');
+// const Path = require('path-parser');
+// const { URL } = require('url');
 
 const requireLogin = require('../middlewares/requireLogin');
 
 const User = mongoose.model('users');
 
 module.exports = app => {
-  app.patch('/api/users/:userId', requireLogin, async (req, res) =>{
-
+  app.patch('/api/users/', requireLogin, async (req, res) =>{
+    User.update({
+      _id: req.user.id },
+      { $set: {
+        username: req.body.username
+        title: req.body.title,                                                        // the user's title
+        city: req.body.city,
+        travelRange: req.body.travelRange,
+        followedGames: req.body.followedGames,
+        followedSeries: req.body.followedSeries,
+        finishAccountSetup: req.body.finishAccountSetup,
+        tournaments: req.body.tournaments
+        }
+      }, //callback for error handling and immediate execution
+      (err) => {
+        if(err) {
+          res.status(422).send(err);
+        }
+      }
+    )
+    res.send(req.user);
   })
 
-  app.delete('/api/users/:userId', requireLogin, async (req, res) =>{
+  app.delete('/api/users/', requireLogin, async (req, res) =>{
 
   })
 };
