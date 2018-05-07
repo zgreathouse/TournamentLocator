2c8a71e26d7041e4130ec5c532feae29e9c40d71
zgreathouse <greathouse.zachary@gmail.com>
2018-05-06 22:35:10 -0700

    update devNotes.js file

--- frontend/src/util/devNotes.js
+++ frontend/src/util/devNotes.js
@@ -1,23 +1,8 @@
 /*
 TODO
 
-  * create user profile page
-    * create link on page to edit profile
-  * create user edit form
-    * create multi input (require commas is not cutting it)
-  * create link to user profile in header
-  * create redux cycle for editing and deleting users
-  * create delete account button option
-
-
-  * Create Post edit form
-    * set up edit form to be prepopulated with existing values
-
-  *Finish Forum architecure
-    *show date in words on posts
-
-  *Create comments section
-    *add date in words on comments
+  *Bug hunting across app
+    - render verification message when canceling from the tournament form
 
   *Review all components and DRY out where necessary
 */
