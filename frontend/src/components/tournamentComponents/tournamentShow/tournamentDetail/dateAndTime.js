12807c05cbfc49ee5f91ad2ae9e0b8639185ebda
zgreathouse <greathouse.zachary@gmail.com>
2018-05-17 19:27:03 -0700

    further adjustment to how date is rendered on show page

--- frontend/src/components/tournamentComponents/tournamentShow/tournamentDetail/dateAndTime.js
+++ frontend/src/components/tournamentComponents/tournamentShow/tournamentDetail/dateAndTime.js
@@ -10,14 +10,16 @@ const DateAndTime = ({ tournament }) => {
   let date;
   let start;
   let end;
+  let label;
 
   startTime ? date = extractDate(startTime) : date = seriesDay;
   startTime ? start = extractTime(startTime) : start = convertTo12HourTime(seriesStartTime);
   endTime ? end = extractTime(endTime) : end = convertTo12HourTime(seriesEndTime) ;
+  endTime ? label = "Date" : label = "Weekday";
 
   return (
     <div>
-      <div>Date: {date}</div>
+      <div>{label}: {date}</div>
       <div>
         <div>Start: {start} </div>
         <div>End: {end} </div>
