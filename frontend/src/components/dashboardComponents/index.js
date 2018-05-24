259d622accb01a735b65efaa60bb8eaa171a3f53
zgreathouse <greathouse.zachary@gmail.com>
2018-05-24 16:25:53 -0700

    minor style changes to dashboard and landing page skeletons

--- frontend/src/components/dashboardComponents/index.js
+++ frontend/src/components/dashboardComponents/index.js
@@ -4,14 +4,14 @@ import { connect } from 'react-redux';
 const Dashboard = ({ currentUser }) => {
   if (!currentUser) {
     return (
-      <h3 style={{margin: '40px'}}>
+      <h3 style={{margin: '20px 40px'}}>
         Loading...
       </h3>
     )
   }
 
   return (
-    <h3 style={{margin: '40px'}}>
+    <h3 style={{margin: '20px 40px'}}>
       Welcome, {currentUser.username}
     </h3>
   )
--- frontend/src/components/landingPageComponents/index.js
+++ frontend/src/components/landingPageComponents/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LandingPage = () => {
   return (
-    <h3 style={{margin: '40px'}}>
+    <h3 style={{margin: '20px'}}>
       BATL = Bay Area Tournament Locator
     </h3>
   )
