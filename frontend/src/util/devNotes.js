4bc500f7d4fd3461392e033a141c45ca7d6b2242
zgreathouse <greathouse.zachary@gmail.com>
2018-05-27 15:24:55 -0700

    updated the dev notes file

--- frontend/src/util/devNotes.js
+++ frontend/src/util/devNotes.js
@@ -1,8 +1,10 @@
 /*
 TODO
 
+  ** make body field on post form a required field, error message not showing
+
+
   *Bug hunting across app
   *DRY out form components where necessary
-    - section descriptions
 
 */
