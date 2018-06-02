69fd0833262a5961950bd3afb22f69c39fd4833a
zgreathouse <greathouse.zachary@gmail.com>
2018-06-02 12:05:40 -0700

    minor style changes to auth modal

--- frontend/src/components/headerComponents/auth/AuthModal.js
+++ frontend/src/components/headerComponents/auth/AuthModal.js
@@ -12,8 +12,8 @@ const customStyles = {
     bottom           : "auto",
     marginRight      : "-50%",
     transform        : "translate(-50%, -50%)",
-    width            : "400px",
-    height           : "480px"
+    width            : "380px",
+    height           : "420px"
   }
 };
 
--- frontend/src/components/headerComponents/auth/AuthModalBody.js
+++ frontend/src/components/headerComponents/auth/AuthModalBody.js
@@ -3,12 +3,12 @@ import React from 'react';
 //components
 import OAuthButton from './oauthButton';
 
-const AuthModalBody = ({modalType}) => {
+const AuthModalBody = ({ modalType }) => {
   return (
     <div className="modal-content">
-      <h2 className="modal-greeting">
+      <h3 className="modal-greeting" >
         {modalType} with <br/> Google or Facebook!
-      </h2>
+      </h3>
       <div className="oauth-button-container">
         <OAuthButton modalType={modalType} authenticator="google"/>
         <OAuthButton modalType={modalType} authenticator="facebook"/>
