a084eb305446fbbcb223c8d99b67b2df9490674b
zgreathouse <greathouse.zachary@gmail.com>
2018-06-04 08:52:24 -0700

    minor css style changes throughout app regarding color

--- frontend/src/components/formComponents/tournamentForm/sectionTextGuides/generalSection.js
+++ frontend/src/components/formComponents/tournamentForm/sectionTextGuides/generalSection.js
@@ -5,7 +5,7 @@ const GeneralSection = () => {
     <div className='section-text-container'>
       <p className="section-text" style={{width: "900px", margin: "0 0 20px 10px"}}>
           Specify the title and featured game of your tournament here, along with maximum capacity of entrants.<br/>
-          Additionally, remember to add a lot oftags to make your tournament easier to find! <br/>
+          Additionally, remember to add a lot of tags to make your tournament easier to find! <br/>
       </p>
     </div>
   )
