1ce8ca766e5718ea1682046883017a0c105c661d
zgreathouse <greathouse.zachary@gmail.com>
2018-06-06 13:56:02 -0700

    update developer notes

--- frontend/src/util/devNotes.js
+++ frontend/src/util/devNotes.js
@@ -1,8 +1,10 @@
 /*
 TODO
 
-  ** fix the post form modal
+
   ** extract the inline styling on the tournament components to make components more modular
+  ** get google maps api key
+  ** make google maps components
 
   *Bug hunting across app
   *DRY out form components where necessary
