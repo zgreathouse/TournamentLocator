8704bd9c8a8f15be311987e0e1714ae1c9de14ef
zgreathouse <greathouse.zachary@gmail.com>
2018-06-10 23:24:55 -0700

    have tournament links ection of tournament showpage render conditionally based on whether links were provided or not

--- frontend/src/components/tournamentComponents/tournamentShow/tournamentDetail/tournamentLinks.js
+++ frontend/src/components/tournamentComponents/tournamentShow/tournamentDetail/tournamentLinks.js
@@ -3,6 +3,10 @@ import React from 'react';
 const TournamentLinks = ({ tournament }) => {
   const { streamLink, twitterLink } = tournament;
 
+  if (!streamLink && !twitterLink) {
+    return <div></div>
+  }
+
   let stream = streamLink ? <div><strong>Stream:</strong> {streamLink}</div> : <div></div>
   let twitter = twitterLink ? <div><strong>Twitter:</strong> {twitterLink}</div> : <div></div>
 
