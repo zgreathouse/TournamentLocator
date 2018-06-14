f1b45e1a0699c5ce536d8af2351ae625bd72e3dd
zgreathouse <greathouse.zachary@gmail.com>
2018-06-13 23:43:50 -0700

    update devNotes.js

--- frontend/src/util/devNotes.js
+++ frontend/src/util/devNotes.js
@@ -1,9 +1,17 @@
 /*
 TODO
 
-  ** extract the inline styling on the tournament components to make components more modular
+  ** extract the inline styling on the tournament components to make
+     components more modular
   ** get google maps api key
   ** make google maps components
+
+  ** get a large map view on the dashboard
+  ** small map view on the tournament show Page
+  ** style show Page
+
+  ** get a tentative default image to put banners where tournmanet
+     banners will be (Index, Show Page, Forum)
 
   *Bug hunting across app
   *DRY out form components where necessary
